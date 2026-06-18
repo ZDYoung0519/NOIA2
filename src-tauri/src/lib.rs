58d5b6a326ef24ab387ffd4a4676afacf07a6154
ZDYoung0519 <806032098@qq.com>
2026-06-18 13:06:10 +0800

    v3.0.8 自动更新测试

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -51,6 +51,9 @@ pub fn run() {
             }
         }))
         .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_deep_link::init())
+        .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_notification::init())
         .plugin(plugins::shortcut::global_shortcut_plugin())
         .plugin(plugins::shortcut::init())
